1daf142998434260b15bc466d081049a0249bf2e
captainmagellan <captainmagellan@outlook.fr>
2017-10-25 11:21:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var input = document.querySelector("input");
+var button = document.querySelector("#someButton");
+var p = document.querySelector(".bigHead");
+var inputFirstP = document.querySelector("#inputFirstP")
+
+input.addEventListener("change", function(event){
+  p.textContent = "Hi " + event.target.value + ", welcome to my front-end experiments page";
+  button.style.display = "none";
+  console.log(button);
+  input.style.display = "none";
+  inputFirstP.style.display = "none";
+});
+
+//alert("Doesn't actually supports Internet Explorer, be sure to use chrome, firefox, opera, or another modern browser")
